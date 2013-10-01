207443d7023b02b97c5003fd3fabddf5bf5b7ba0
Drew Barnes <andrew@meltwater.org>
2013-10-01 00:39:03 +0000

    Up to date

---
+++ DemoMain.java
@@ -0,0 +1,19 @@
+public class DemoMain
+{
+
+	public static void main(String[] args) {
+	
+		Backend backend = new Backend();
+
+		backend.userMenu();
+
+		// if (backend.validateNameWithSpaces("Meltwater Ent. Sch. of Tech")){
+		// 	System.out.println("it matches");
+		// } else {
+		// 	System.out.println("it doesn't match");
+		// }
+
+	}
+	
+
+}
